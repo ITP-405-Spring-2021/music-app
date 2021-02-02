feefc9ee13206953340f783027fdaeadf4da2325
David Tang <dtang859@gmail.com>
2021-02-01 19:44:33 -0800

    Week 3

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\InvoiceController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/invoices', [InvoiceController::class, 'index'])->name('invoice.index');
+Route::get('/invoices/{id}', [InvoiceController::class, 'show'])->name('invoice.show');
