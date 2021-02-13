73eda3c30943af2760877675159571c053feb9a6
David Tang <dtang859@gmail.com>
2021-02-13 14:34:54 -0800

    Week 6.1 - Set up Eloquent models and examples at /eloquent

---
+++ resources/views/eloquent.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main')
+
+@section('title', 'Eloquent Playground')
+
+@section('content')
+
+{{-- EAGER LOADING BELONGS TO RELATIONSHIPS --}}
+@foreach ($tracks as $track)
+    <div>
+        {{ $track->name }}
+        {{ $track->genre->name }}
+        {{ $track->album->title }}
+    </div>
+@endforeach
+
+@endsection
