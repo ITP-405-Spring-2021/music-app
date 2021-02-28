4f075955b75e5a48d9999220ecbb005c8d07d83c
David Tang <dtang859@gmail.com>
2021-02-28 14:06:37 -0800

    Week 7.2 - Logout

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AlbumController;
 use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\RegistrationController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Track;
 use App\Models\Artist;
@@ -36,7 +37,7 @@ Route::post('/albums/{id}', [AlbumController::class, 'update'])->name('albums.up
 Route::get('/register', [RegistrationController::class, 'index'])->name('registration.index');
 Route::post('/register', [RegistrationController::class, 'register'])->name('registration.create');
 Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
-
+Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
 
 Route::get('/eloquent', function() {
     // QUERYING
