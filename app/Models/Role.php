8f22730d3f24b27bc19c4f442c72f2d84e364352
David Tang <dtang859@gmail.com>
2021-03-06 10:04:21 -0800

    Week 9.2 - Setting a user's role during registration
    Associating Belongs To relationships: https://laravel.com/docs/8.x/eloquent-relationships#updating-belongs-to-relationships

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -10,4 +10,9 @@ class Role extends Model
     use HasFactory;
 
     protected $fillable = ['slug', 'name'];
+
+    public static function getUser()
+    {
+        return self::where('slug', '=', 'user')->first();
+    }
 }
