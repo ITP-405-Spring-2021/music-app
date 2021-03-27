5ae854b428f66008f025d438822d81432f309095
David Tang <dtang859@gmail.com>
2021-03-26 20:59:51 -0700

    Week 11.4 - Dispatching Jobs
    ## Commands
    
    ```
    php artisan make:job AnnounceNewAlbum
    ```

---
+++ app/Jobs/AnnounceNewAlbum.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Album;
+use App\Models\User;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\NewAlbum;
+
+class AnnounceNewAlbum implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $album;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Album $album)
+    {
+        $this->album = $album;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $users = User::all();
+
+        foreach ($users as $user) {
+            Mail::to($user->email)->send(new NewAlbum($this->album));
+        }
+    }
+}
